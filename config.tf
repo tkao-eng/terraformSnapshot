4f7864ab9576ae79bc15208e01d6e76b0926bc62
Thomas Kao <tkao@sumologic.com>
2019-09-22 15:52:03 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000002_1569192719961/config.tf

--- config.tf
+++ config.tf
@@ -2,73 +2,541 @@ resource "sumologic_user" "terraformfirstNamelastNameUser" {
     firstName = firstName
      lastName = lastName
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformCollectorRegistrationUser" {
     firstName = Collector
      lastName = Registration
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformJapneetChawlaUser" {
     firstName = Japneet
      lastName = Chawla
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformSylvainRoyerUser" {
     firstName = Sylvain
      lastName = Royer
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformJacekMigdalUser" {
     firstName = Jacek
      lastName = Migdal
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformAdamSznajderUser" {
     firstName = Adam
      lastName = Sznajder
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformTingXiongUser" {
     firstName = Ting
      lastName = Xiong
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformKumarAvijitUser" {
     firstName = Kumar
      lastName = Avijit
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformMattLarsenUser" {
     firstName = Matt
      lastName = Larsen
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformSaubhagyaMaheshwariUser" {
     firstName = Saubhagya
      lastName = Maheshwari
      isActive = true
-     roleIds = [0000000000000001]
+     roleIds = [0000000000000003]
 }
 resource "sumologic_user" "terraformRafalRozakUser" {
     firstName = Rafal
      lastName = Rozak
      isActive = true
-     roleIds = []
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformHimanshuShekharUser" {
+    firstName = Himanshu
+     lastName = Shekhar
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformAleksanderKramarzUser" {
+    firstName = Aleksander
+     lastName = Kramarz
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformDavidBlondeauUser" {
+    firstName = David
+     lastName = Blondeau
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMarcinChwedczukUser" {
+    firstName = Marcin
+     lastName = Chwedczuk
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformOmidMortazaviUser" {
+    firstName = Omid
+     lastName = Mortazavi
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformCharlesQiUser" {
+    firstName = Charles
+     lastName = Qi
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformSeanMcKibbenUser" {
+    firstName = Sean
+     lastName = McKibben
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformVishalSahUser" {
+    firstName = Vishal
+     lastName = Sah
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformBillLazarUser" {
+    firstName = Bill
+     lastName = Lazar
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformStevenLiUser" {
+    firstName = Steven
+     lastName = Li
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformhimanshupalUser" {
+    firstName = himanshu
+     lastName = pal
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformYuanChenUser" {
+    firstName = Yuan
+     lastName = Chen
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformBartekEtenkowskiUser" {
+    firstName = Bartek
+     lastName = Etenkowski
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformSakshiJainUser" {
+    firstName = Sakshi
+     lastName = Jain
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMaciejZbierskiUser" {
+    firstName = Maciej
+     lastName = Zbierski
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMingjunYangUser" {
+    firstName = Mingjun
+     lastName = Yang
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformGouravGargUser" {
+    firstName = Gourav
+     lastName = Garg
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMarkShanUser" {
+    firstName = Mark
+     lastName = Shan
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformgurvindersinghUser" {
+    firstName = gurvinder
+     lastName = singh
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformFilipOchnikUser" {
+    firstName = Filip
+     lastName = Ochnik
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformsaurabhvaidyaUser" {
+    firstName = saurabh
+     lastName = vaidya
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformLeiHuangUser" {
+    firstName = Lei
+     lastName = Huang
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformAdityaPatelUser" {
+    firstName = Aditya
+     lastName = Patel
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMattSmithUser" {
+    firstName = Matt
+     lastName = Smith
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMichalMatusiakUser" {
+    firstName = Michal
+     lastName = Matusiak
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformCodyRecenoUser" {
+    firstName = Cody
+     lastName = Receno
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformSayilMedinaUser" {
+    firstName = Sayil
+     lastName = Medina
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformGauravJainUser" {
+    firstName = Gaurav
+     lastName = Jain
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformXiLiUser" {
+    firstName = Xi
+     lastName = Li
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformVarunMainiUser" {
+    firstName = Varun
+     lastName = Maini
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformKasiaMacioszekUser" {
+    firstName = Kasia
+     lastName = Macioszek
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformBarakJacobUser" {
+    firstName = Barak
+     lastName = Jacob
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPratikIndapUser" {
+    firstName = Pratik
+     lastName = Indap
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformAshishRanjanUser" {
+    firstName = Ashish
+     lastName = Ranjan
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformamangoelUser" {
+    firstName = aman
+     lastName = goel
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPiotrStyczynskiUser" {
+    firstName = Piotr
+     lastName = Styczynski
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPrashantTrivediUser" {
+    firstName = Prashant
+     lastName = Trivedi
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMatthewKimUser" {
+    firstName = Matthew
+     lastName = Kim
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformVikasSrivastavaUser" {
+    firstName = Vikas
+     lastName = Srivastava
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPaulRogersUser" {
+    firstName = Paul
+     lastName = Rogers
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformKarlSchumakerUser" {
+    firstName = Karl
+     lastName = Schumaker
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformAmanSinghalUser" {
+    firstName = Aman
+     lastName = Singhal
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformJannatWattsUser" {
+    firstName = Jannat
+     lastName = Watts
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPriyanshGoelUser" {
+    firstName = Priyansh
+     lastName = Goel
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformMarcinDominiakUser" {
+    firstName = Marcin
+     lastName = Dominiak
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformDmitryUdalovUser" {
+    firstName = Dmitry
+     lastName = Udalov
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformRishulAggarwalUser" {
+    firstName = Rishul
+     lastName = Aggarwal
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformTejaVojjalaUser" {
+    firstName = Teja
+     lastName = Vojjala
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformManikSinglaUser" {
+    firstName = Manik
+     lastName = Singla
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformKittyCaoUser" {
+    firstName = Kitty
+     lastName = Cao
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformAdityaSuleUser" {
+    firstName = Aditya
+     lastName = Sule
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformReetikaRoyUser" {
+    firstName = Reetika
+     lastName = Roy
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformTusharkBansalUser" {
+    firstName = Tushark
+     lastName = Bansal
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformChrisDessonvilleUser" {
+    firstName = Chris
+     lastName = Dessonville
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformPiotrGalarUser" {
+    firstName = Piotr
+     lastName = Galar
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformSamSongUser" {
+    firstName = Sam
+     lastName = Song
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformFalakKansalUser" {
+    firstName = Falak
+     lastName = Kansal
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformankitsinglaUser" {
+    firstName = ankit
+     lastName = singla
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformArvindhSwamiUser" {
+    firstName = Arvindh
+     lastName = Swami
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformKeshavGuptaUser" {
+    firstName = Keshav
+     lastName = Gupta
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformTomaszFalkiewiczUser" {
+    firstName = Tomasz
+     lastName = Falkiewicz
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformCarterTuneUser" {
+    firstName = Carter
+     lastName = Tune
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformDhivagarGnanaratnamUser" {
+    firstName = Dhivagar
+     lastName = Gnanaratnam
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformTanayJhaUser" {
+    firstName = Tanay
+     lastName = Jha
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformJohnSardoUser" {
+    firstName = John
+     lastName = Sardo
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformLukaszChodarcewiczUser" {
+    firstName = Lukasz
+     lastName = Chodarcewicz
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformStefanZierUser" {
+    firstName = Stefan
+     lastName = Zier
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformDivyaTripathiUser" {
+    firstName = Divya
+     lastName = Tripathi
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformEladMichaeliUser" {
+    firstName = Elad
+     lastName = Michaeli
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformHarishSharmaUser" {
+    firstName = Harish
+     lastName = Sharma
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformHatimBasra WalaUser" {
+    firstName = Hatim
+     lastName = Basra Wala
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformKevinLiuUser" {
+    firstName = Kevin
+     lastName = Liu
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformThomasKaoUser" {
+    firstName = Thomas
+     lastName = Kao
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformStevenBaldwinUser" {
+    firstName = Steven
+     lastName = Baldwin
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformGarrettNanoUser" {
+    firstName = Garrett
+     lastName = Nano
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformRyleyHigaUser" {
+    firstName = Ryley
+     lastName = Higa
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformWojciechDonderowiczUser" {
+    firstName = Wojciech
+     lastName = Donderowicz
+     isActive = true
+     roleIds = [0000000000000003]
+}
+resource "sumologic_user" "terraformHanlinLiUser" {
+    firstName = Hanlin
+     lastName = Li
+     isActive = true
+     roleIds = [0000000000000003]
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = Administrator
     description = 
     filterPredicate = *
-    users = [0000000000000001, 0000000000000007, 000000000000000A, 000000000000000C, 000000000000000E, 0000000000000011, 0000000000000016, 0000000000000018, 000000000000001C, 000000000000001F, 0000000000000022, 0000000000000025, 0000000000000028, 000000000000002B, 000000000000002D, 0000000000000031, 0000000000000034, 0000000000000037, 000000000000003A, 000000000000003D, 000000000000003F, 0000000000000043, 0000000000000045, 0000000000000048, 000000000000004B, 000000000000004F, 0000000000000052, 0000000000000055, 0000000000000058, 000000000000005B, 000000000000005E, 0000000000000061, 0000000000000064, 0000000000000067, 0000000000000068, 000000000000006B, 000000000000006E, 0000000000000071, 0000000000000076, 0000000000000077, 000000000000007C, 000000000000007D, 0000000000000082, 0000000000000083, 0000000000000086, 000000000000008A, 000000000000008D, 000000000000008F, 0000000000000093, 0000000000000095, 000000000000009A]
+    users = [0000000000000002, 0000000000000005, 0000000000000009, 000000000000000D, 0000000000000010, 0000000000000012, 0000000000000015, 0000000000000019, 000000000000001B, 000000000000001D, 0000000000000020, 0000000000000023, 0000000000000027, 000000000000002A, 000000000000002C, 0000000000000030, 0000000000000032, 0000000000000036, 0000000000000039, 000000000000003B, 000000000000003E, 0000000000000041, 0000000000000046, 0000000000000049, 000000000000004C, 000000000000004E, 0000000000000050, 0000000000000054, 0000000000000057, 000000000000005A, 000000000000005C, 000000000000005F, 0000000000000063, 0000000000000065, 000000000000006A, 000000000000006D, 0000000000000070, 0000000000000073, 0000000000000074, 0000000000000078, 000000000000007A, 000000000000007E, 0000000000000081, 0000000000000085, 0000000000000088, 000000000000008B, 000000000000008C, 0000000000000091, 0000000000000092, 0000000000000097, 0000000000000098, 000000000000009C, 000000000000009F, 00000000000000A2, 00000000000000A6, 00000000000000A8, 00000000000000AB, 00000000000000AE, 00000000000000B0, 00000000000000B3, 00000000000000B8, 00000000000000BB, 00000000000000BE, 00000000000000BF, 00000000000000C4, 00000000000000C7, 00000000000000C9, 00000000000000CC, 00000000000000CF, 00000000000000D1, 00000000000000D5, 00000000000000D8, 00000000000000DC, 00000000000000DD, 00000000000000E1, 00000000000000E5, 00000000000000E8, 00000000000000EA, 00000000000000EE, 00000000000000EF, 00000000000000F2, 00000000000000F5, 00000000000000F8, 00000000000000FC, 00000000000000FF, 0000000000000101, 0000000000000104, 0000000000000108, 000000000000010A]
     capabilities = [dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding]
 }
 resource "sumologic_role" "terraformAnalystRole" {
