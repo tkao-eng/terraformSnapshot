1cfc82afb34dbba6a20d3f07fe8760154031e998
api <api@frontend-1>
2019-09-22 22:20:03 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_000000000000000E_1569216000221/config_TestOrg-scheduledviewmanagementapiit-69tr1au6tkes45sw.tf

---
+++ config_TestOrg-scheduledviewmanagementapiit-69tr1au6tkes45sw.tf
@@ -0,0 +1,32 @@
+provider "sumologic" {
+    access_id = "suH6J0GEtOBFwW"
+    access_key = "kpBpgTjCoyf71FxzwnC5QxRzMDACa4MSxUo39eo4aYvX38xJGP9JOid3rNOcMfKn"
+    environment = "tkao"
+}
+
+resource "sumologic_user" "terraformAdminUserUser" {
+    firstName = "Admin"
+     lastName = "User"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@c946d64
+}
+resource "sumologic_user" "terraformtestuserUser" {
+    firstName = "test"
+     lastName = "user"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1e4d17c8
+}
+resource "sumologic_role" "terraformAdministratorRole" {
+    name = "Administrator"
+    description = ""
+    filterPredicate = "*"
+    users = [Ljava.lang.String;@50c2b5c7
+    capabilities = [Ljava.lang.String;@31a0ef98
+}
+resource "sumologic_role" "terraformAnalystRole" {
+    name = "Analyst"
+    description = ""
+    filterPredicate = ""
+    users = [Ljava.lang.String;@3eae71e0
+    capabilities = [Ljava.lang.String;@186103d8
+}
