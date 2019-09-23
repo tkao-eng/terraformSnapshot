b339d4b5348637a36122c9b98741ffa0d4f7b426
api <api@frontend-1>
2019-09-22 23:41:40 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000016_1569220897099/config_TestOrg-evenAnotherOrg-pyyk8extii6ymlqw.tf

--- config_TestOrg-evenAnotherOrg-pyyk8extii6ymlqw.tf
+++ config_TestOrg-evenAnotherOrg-pyyk8extii6ymlqw.tf
@@ -8,19 +8,19 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = "Admin"
      lastName = "User"
      isActive = "true"
-     roleIds = 000000000000002B
+     roleIds = [000000000000002B]
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = "Administrator"
     description = ""
     filterPredicate = "*"
-    users = 00000000000001F1
-    capabilities = dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding
+    users = [00000000000001F1]
+    capabilities = [dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding]
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = "Analyst"
     description = ""
     filterPredicate = ""
-    users = 
-    capabilities = viewCollectors
+    users = []
+    capabilities = [viewCollectors]
 }
