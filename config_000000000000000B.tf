a0654a1202fb096c80934bdea65de32a246a7474
api <api@frontend-1>
2019-09-22 19:53:35 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_000000000000000B_1569207212752/config_000000000000000B.tf

---
+++ config_000000000000000B.tf
@@ -0,0 +1,32 @@
+provider "sumologic" {
+    access_id = "suH6J0GEtOBFwW"
+    access_key = "kpBpgTjCoyf71FxzwnC5QxRzMDACa4MSxUo39eo4aYvX38xJGP9JOid3rNOcMfKn"
+    environment = "tkao"
+}
+
+resource "sumologic_user" "terraformAdminUserUser" {
+    firstName = Admin
+     lastName = User
+     isActive = true
+     roleIds = [0000000000000015]
+}
+resource "sumologic_user" "terraformtestuserUser" {
+    firstName = test
+     lastName = user
+     isActive = true
+     roleIds = [0000000000000016]
+}
+resource "sumologic_role" "terraformAdministratorRole" {
+    name = Administrator
+    description = 
+    filterPredicate = *
+    users = [00000000000001DC]
+    capabilities = [dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding]
+}
+resource "sumologic_role" "terraformAnalystRole" {
+    name = Analyst
+    description = 
+    filterPredicate = 
+    users = [00000000000001DD]
+    capabilities = [viewCollectors]
+}
