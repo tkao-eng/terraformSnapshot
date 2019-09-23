b0f3cb935461231747c612046820fe048dfe10b5
api <api@frontend-1>
2019-09-22 22:28:42 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000015_1569216519463/config_TestOrg-anotherOrg-zetit49j9fababd4.tf

---
+++ config_TestOrg-anotherOrg-zetit49j9fababd4.tf
@@ -0,0 +1,26 @@
+provider "sumologic" {
+    access_id = "suH6J0GEtOBFwW"
+    access_key = "kpBpgTjCoyf71FxzwnC5QxRzMDACa4MSxUo39eo4aYvX38xJGP9JOid3rNOcMfKn"
+    environment = "tkao"
+}
+
+resource "sumologic_user" "terraformAdminUserUser" {
+    firstName = "Admin"
+     lastName = "User"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@40619495
+}
+resource "sumologic_role" "terraformAdministratorRole" {
+    name = "Administrator"
+    description = ""
+    filterPredicate = "*"
+    users = [Ljava.lang.String;@4c221d98
+    capabilities = [Ljava.lang.String;@4bae6149
+}
+resource "sumologic_role" "terraformAnalystRole" {
+    name = "Analyst"
+    description = ""
+    filterPredicate = ""
+    users = [Ljava.lang.String;@3ab23e4b
+    capabilities = [Ljava.lang.String;@8aa53fc
+}
