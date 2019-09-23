5856afb8753b742d45f0c46d1c28669aaf9e9558
api <api@frontend-1>
2019-09-23 00:50:22 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_000000000000000B_1569225019088/config_TestOrg-scheduledviewmanagementapiit-p5oz62wj6b395v47.tf

--- config_TestOrg-scheduledviewmanagementapiit-p5oz62wj6b395v47.tf
+++ config_TestOrg-scheduledviewmanagementapiit-p5oz62wj6b395v47.tf
@@ -8,25 +8,25 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = "Admin"
      lastName = "User"
      isActive = "true"
-     roleIds = [Ljava.lang.String;@1d5e13da
+     roleIds = [Ljava.lang.String;@103e7a06
 }
 resource "sumologic_user" "terraformtestuserUser" {
     firstName = "test"
      lastName = "user"
      isActive = "true"
-     roleIds = [Ljava.lang.String;@5daaf1d7
+     roleIds = [Ljava.lang.String;@144f2593
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = "Administrator"
     description = ""
     filterPredicate = "*"
-    users = [Ljava.lang.String;@44ea77cd
-    capabilities = [Ljava.lang.String;@3aee1a64
+    users = [Ljava.lang.String;@48590530
+    capabilities = [Ljava.lang.String;@321ac5d0
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = "Analyst"
     description = ""
     filterPredicate = ""
-    users = [Ljava.lang.String;@4ecbd319
-    capabilities = [Ljava.lang.String;@5252f07c
+    users = [Ljava.lang.String;@67833362
+    capabilities = [Ljava.lang.String;@25a63fec
 }
