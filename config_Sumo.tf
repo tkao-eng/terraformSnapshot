1a287844fc4ab0c6c782a87a84384018e523bd43
api <api@frontend-1>
2019-09-22 22:18:17 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000001_1569215894003/config_Sumo.tf

---
+++ config_Sumo.tf
@@ -0,0 +1,620 @@
+provider "sumologic" {
+    access_id = "suH6J0GEtOBFwW"
+    access_key = "kpBpgTjCoyf71FxzwnC5QxRzMDACa4MSxUo39eo4aYvX38xJGP9JOid3rNOcMfKn"
+    environment = "tkao"
+}
+
+resource "sumologic_user" "terraformfirstNamelastNameUser" {
+    firstName = "firstName"
+     lastName = "lastName"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6f3325e
+}
+resource "sumologic_user" "terraformCollectorRegistrationUser" {
+    firstName = "Collector"
+     lastName = "Registration"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@86d2613
+}
+resource "sumologic_user" "terraformJapneetChawlaUser" {
+    firstName = "Japneet"
+     lastName = "Chawla"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@10aa3632
+}
+resource "sumologic_user" "terraformRamanBahdanovichUser" {
+    firstName = "Raman"
+     lastName = "Bahdanovich"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1639dc1c
+}
+resource "sumologic_user" "terraformSylvainRoyerUser" {
+    firstName = "Sylvain"
+     lastName = "Royer"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@37504f8c
+}
+resource "sumologic_user" "terraformJacekMigdalUser" {
+    firstName = "Jacek"
+     lastName = "Migdal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@64ad5a3a
+}
+resource "sumologic_user" "terraformAdamSznajderUser" {
+    firstName = "Adam"
+     lastName = "Sznajder"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@49981eec
+}
+resource "sumologic_user" "terraformTingXiongUser" {
+    firstName = "Ting"
+     lastName = "Xiong"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@336df7d4
+}
+resource "sumologic_user" "terraformArpitJainUser" {
+    firstName = "Arpit"
+     lastName = "Jain"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2982ae75
+}
+resource "sumologic_user" "terraformKumarAvijitUser" {
+    firstName = "Kumar"
+     lastName = "Avijit"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@73f725fd
+}
+resource "sumologic_user" "terraformMattLarsenUser" {
+    firstName = "Matt"
+     lastName = "Larsen"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@fabe773
+}
+resource "sumologic_user" "terraformSaubhagyaMaheshwariUser" {
+    firstName = "Saubhagya"
+     lastName = "Maheshwari"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@36c5c231
+}
+resource "sumologic_user" "terraformHimanshuShekharUser" {
+    firstName = "Himanshu"
+     lastName = "Shekhar"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@142269b0
+}
+resource "sumologic_user" "terraformAleksanderKramarzUser" {
+    firstName = "Aleksander"
+     lastName = "Kramarz"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@44731ecc
+}
+resource "sumologic_user" "terraformDavidBlondeauUser" {
+    firstName = "David"
+     lastName = "Blondeau"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@311d21ac
+}
+resource "sumologic_user" "terraformMarcinChwedczukUser" {
+    firstName = "Marcin"
+     lastName = "Chwedczuk"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7243fb96
+}
+resource "sumologic_user" "terraformOmidMortazaviUser" {
+    firstName = "Omid"
+     lastName = "Mortazavi"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@250d1152
+}
+resource "sumologic_user" "terraformCharlesQiUser" {
+    firstName = "Charles"
+     lastName = "Qi"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@677b1aef
+}
+resource "sumologic_user" "terraformVishalSahUser" {
+    firstName = "Vishal"
+     lastName = "Sah"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1b6a6c0
+}
+resource "sumologic_user" "terraformBillLazarUser" {
+    firstName = "Bill"
+     lastName = "Lazar"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7d0adbd8
+}
+resource "sumologic_user" "terraformStevenLiUser" {
+    firstName = "Steven"
+     lastName = "Li"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@be902c8
+}
+resource "sumologic_user" "terraformhimanshupalUser" {
+    firstName = "himanshu"
+     lastName = "pal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@47c86fc9
+}
+resource "sumologic_user" "terraformBartekEtenkowskiUser" {
+    firstName = "Bartek"
+     lastName = "Etenkowski"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4e952
+}
+resource "sumologic_user" "terraformSakshiJainUser" {
+    firstName = "Sakshi"
+     lastName = "Jain"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@269fbc10
+}
+resource "sumologic_user" "terraformMaciejZbierskiUser" {
+    firstName = "Maciej"
+     lastName = "Zbierski"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2223c2f2
+}
+resource "sumologic_user" "terraformGouravGargUser" {
+    firstName = "Gourav"
+     lastName = "Garg"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@c875e63
+}
+resource "sumologic_user" "terraformgurvindersinghUser" {
+    firstName = "gurvinder"
+     lastName = "singh"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@ac6303b
+}
+resource "sumologic_user" "terraformFilipOchnikUser" {
+    firstName = "Filip"
+     lastName = "Ochnik"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4a504252
+}
+resource "sumologic_user" "terraformLeiHuangUser" {
+    firstName = "Lei"
+     lastName = "Huang"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@568c292f
+}
+resource "sumologic_user" "terraformKevinKoUser" {
+    firstName = "Kevin"
+     lastName = "Ko"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@360fb10c
+}
+resource "sumologic_user" "terraformAdityaPatelUser" {
+    firstName = "Aditya"
+     lastName = "Patel"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@63062701
+}
+resource "sumologic_user" "terraformMattSmithUser" {
+    firstName = "Matt"
+     lastName = "Smith"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@74317860
+}
+resource "sumologic_user" "terraformMichalMatusiakUser" {
+    firstName = "Michal"
+     lastName = "Matusiak"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1567cd69
+}
+resource "sumologic_user" "terraformSayilMedinaUser" {
+    firstName = "Sayil"
+     lastName = "Medina"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5423ce93
+}
+resource "sumologic_user" "terraformGauravJainUser" {
+    firstName = "Gaurav"
+     lastName = "Jain"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5462e394
+}
+resource "sumologic_user" "terraformXiLiUser" {
+    firstName = "Xi"
+     lastName = "Li"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1a671b54
+}
+resource "sumologic_user" "terraformVarunMainiUser" {
+    firstName = "Varun"
+     lastName = "Maini"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@705f5a50
+}
+resource "sumologic_user" "terraformKasiaMacioszekUser" {
+    firstName = "Kasia"
+     lastName = "Macioszek"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@9363069
+}
+resource "sumologic_user" "terraformAshishRanjanUser" {
+    firstName = "Ashish"
+     lastName = "Ranjan"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6e9a2cb3
+}
+resource "sumologic_user" "terraformamangoelUser" {
+    firstName = "aman"
+     lastName = "goel"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2639295e
+}
+resource "sumologic_user" "terraformPiotrStyczynskiUser" {
+    firstName = "Piotr"
+     lastName = "Styczynski"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@3558b3d0
+}
+resource "sumologic_user" "terraformPrashantTrivediUser" {
+    firstName = "Prashant"
+     lastName = "Trivedi"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4b431eb4
+}
+resource "sumologic_user" "terraformsrujanasrujanaUser" {
+    firstName = "srujana"
+     lastName = "srujana"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2095c4b9
+}
+resource "sumologic_user" "terraformVikasSrivastavaUser" {
+    firstName = "Vikas"
+     lastName = "Srivastava"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7d2b9f81
+}
+resource "sumologic_user" "terraformPaulRogersUser" {
+    firstName = "Paul"
+     lastName = "Rogers"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@705d114b
+}
+resource "sumologic_user" "terraformKarlSchumakerUser" {
+    firstName = "Karl"
+     lastName = "Schumaker"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@cd4e5e2
+}
+resource "sumologic_user" "terraformAmanSinghalUser" {
+    firstName = "Aman"
+     lastName = "Singhal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@67f62a38
+}
+resource "sumologic_user" "terraformJannatWattsUser" {
+    firstName = "Jannat"
+     lastName = "Watts"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@78628150
+}
+resource "sumologic_user" "terraformPriyanshGoelUser" {
+    firstName = "Priyansh"
+     lastName = "Goel"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6263975e
+}
+resource "sumologic_user" "terraformBryceBlantonUser" {
+    firstName = "Bryce"
+     lastName = "Blanton"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1f8b5a4e
+}
+resource "sumologic_user" "terraformDmitryUdalovUser" {
+    firstName = "Dmitry"
+     lastName = "Udalov"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@532da221
+}
+resource "sumologic_user" "terraformRishulAggarwalUser" {
+    firstName = "Rishul"
+     lastName = "Aggarwal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@73f93e7
+}
+resource "sumologic_user" "terraformTejaVojjalaUser" {
+    firstName = "Teja"
+     lastName = "Vojjala"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6bcdc623
+}
+resource "sumologic_user" "terraformManikSinglaUser" {
+    firstName = "Manik"
+     lastName = "Singla"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4968cc11
+}
+resource "sumologic_user" "terraformAdityaSuleUser" {
+    firstName = "Aditya"
+     lastName = "Sule"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@68b636ab
+}
+resource "sumologic_user" "terraformReetikaRoyUser" {
+    firstName = "Reetika"
+     lastName = "Roy"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@68a14074
+}
+resource "sumologic_user" "terraformTusharkBansalUser" {
+    firstName = "Tushark"
+     lastName = "Bansal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@142c44bb
+}
+resource "sumologic_user" "terraformChrisDessonvilleUser" {
+    firstName = "Chris"
+     lastName = "Dessonville"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1cd40381
+}
+resource "sumologic_user" "terraformPiotrGalarUser" {
+    firstName = "Piotr"
+     lastName = "Galar"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7191735e
+}
+resource "sumologic_user" "terraformSamSongUser" {
+    firstName = "Sam"
+     lastName = "Song"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@18dd8fc1
+}
+resource "sumologic_user" "terraformFalakKansalUser" {
+    firstName = "Falak"
+     lastName = "Kansal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@48fb2dcc
+}
+resource "sumologic_user" "terraformankitsinglaUser" {
+    firstName = "ankit"
+     lastName = "singla"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@48afaeaf
+}
+resource "sumologic_user" "terraformArvindhSwamiUser" {
+    firstName = "Arvindh"
+     lastName = "Swami"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@62a1112d
+}
+resource "sumologic_user" "terraformKeshavGuptaUser" {
+    firstName = "Keshav"
+     lastName = "Gupta"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1e8d3bce
+}
+resource "sumologic_user" "terraformTomaszFalkiewiczUser" {
+    firstName = "Tomasz"
+     lastName = "Falkiewicz"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@3d33cfb8
+}
+resource "sumologic_user" "terraformTanayJhaUser" {
+    firstName = "Tanay"
+     lastName = "Jha"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4d86ed17
+}
+resource "sumologic_user" "terraformJohnSardoUser" {
+    firstName = "John"
+     lastName = "Sardo"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5e4c7fea
+}
+resource "sumologic_user" "terraformLukaszChodarcewiczUser" {
+    firstName = "Lukasz"
+     lastName = "Chodarcewicz"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@26cb657b
+}
+resource "sumologic_user" "terraformStefanZierUser" {
+    firstName = "Stefan"
+     lastName = "Zier"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7a212f2e
+}
+resource "sumologic_user" "terraformGokul ReddyMuddarlaUser" {
+    firstName = "Gokul Reddy"
+     lastName = "Muddarla"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5cd4eff0
+}
+resource "sumologic_user" "terraformDivyaTripathiUser" {
+    firstName = "Divya"
+     lastName = "Tripathi"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@47c2a70c
+}
+resource "sumologic_user" "terraformHarishSharmaUser" {
+    firstName = "Harish"
+     lastName = "Sharma"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1a586f8a
+}
+resource "sumologic_user" "terraformCharlieTonneslanUser" {
+    firstName = "Charlie"
+     lastName = "Tonneslan"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4baf643d
+}
+resource "sumologic_user" "terraformHatimBasra WalaUser" {
+    firstName = "Hatim"
+     lastName = "Basra Wala"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@43555055
+}
+resource "sumologic_user" "terraformThomasKaoUser" {
+    firstName = "Thomas"
+     lastName = "Kao"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2c3b1b16
+}
+resource "sumologic_user" "terraformGarrettNanoUser" {
+    firstName = "Garrett"
+     lastName = "Nano"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@30b09e32
+}
+resource "sumologic_user" "terraformRyleyHigaUser" {
+    firstName = "Ryley"
+     lastName = "Higa"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@683c8b0a
+}
+resource "sumologic_user" "terraformWojciechDonderowiczUser" {
+    firstName = "Wojciech"
+     lastName = "Donderowicz"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@553c0eb5
+}
+resource "sumologic_user" "terraformHanlinLiUser" {
+    firstName = "Hanlin"
+     lastName = "Li"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@78e06149
+}
+resource "sumologic_user" "terraformshreyachandraUser" {
+    firstName = "shreya"
+     lastName = "chandra"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2b0976ea
+}
+resource "sumologic_user" "terraformRaymondKwanUser" {
+    firstName = "Raymond"
+     lastName = "Kwan"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@3010ab3b
+}
+resource "sumologic_user" "terraformRyanMillerUser" {
+    firstName = "Ryan"
+     lastName = "Miller"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@521d222b
+}
+resource "sumologic_user" "terraformAndrzejGórskiUser" {
+    firstName = "Andrzej"
+     lastName = "Górski"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5feb6c56
+}
+resource "sumologic_user" "terraformMihirPandyaUser" {
+    firstName = "Mihir"
+     lastName = "Pandya"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4b20eed0
+}
+resource "sumologic_user" "terraformBhartiMadaanUser" {
+    firstName = "Bharti"
+     lastName = "Madaan"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@5231026c
+}
+resource "sumologic_user" "terraformAvinashSharmaUser" {
+    firstName = "Avinash"
+     lastName = "Sharma"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@63826968
+}
+resource "sumologic_user" "terraformBrunoKurticUser" {
+    firstName = "Bruno"
+     lastName = "Kurtic"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2034596b
+}
+resource "sumologic_user" "terraformkrishnaagarwalUser" {
+    firstName = "krishna"
+     lastName = "agarwal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@21a242b4
+}
+resource "sumologic_user" "terraformMaisieWangUser" {
+    firstName = "Maisie"
+     lastName = "Wang"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@61072a45
+}
+resource "sumologic_user" "terraformZbigniewHalasUser" {
+    firstName = "Zbigniew"
+     lastName = "Halas"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4fc530e6
+}
+resource "sumologic_user" "terraformSevaYakhontovUser" {
+    firstName = "Seva"
+     lastName = "Yakhontov"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@3621fea1
+}
+resource "sumologic_user" "terraformChristianBeedgenUser" {
+    firstName = "Christian"
+     lastName = "Beedgen"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@560742f4
+}
+resource "sumologic_user" "terraformaloksharmaUser" {
+    firstName = "alok"
+     lastName = "sharma"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@7fc84500
+}
+resource "sumologic_user" "terraformAayushiBansalUser" {
+    firstName = "Aayushi"
+     lastName = "Bansal"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@2282fe60
+}
+resource "sumologic_user" "terraformYutingLiuUser" {
+    firstName = "Yuting"
+     lastName = "Liu"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6a0f23f9
+}
+resource "sumologic_user" "terraformVaibhavGargUser" {
+    firstName = "Vaibhav"
+     lastName = "Garg"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1fc13c55
+}
+resource "sumologic_user" "terraformLysanderDavidUser" {
+    firstName = "Lysander"
+     lastName = "David"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@69d3493e
+}
+resource "sumologic_user" "terraformabhishekjainUser" {
+    firstName = "abhishek"
+     lastName = "jain"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@1267c49c
+}
+resource "sumologic_user" "terraformFilipMikinaUser" {
+    firstName = "Filip"
+     lastName = "Mikina"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@4a36a78f
+}
+resource "sumologic_user" "terraformRaminSafaieUser" {
+    firstName = "Ramin"
+     lastName = "Safaie"
+     isActive = "true"
+     roleIds = [Ljava.lang.String;@6172d221
+}
+resource "sumologic_role" "terraformAdministratorRole" {
+    name = "Administrator"
+    description = ""
+    filterPredicate = "*"
+    users = [Ljava.lang.String;@78697cb1
+    capabilities = [Ljava.lang.String;@42641594
+}
+resource "sumologic_role" "terraformAnalystRole" {
+    name = "Analyst"
+    description = ""
+    filterPredicate = ""
+    users = [Ljava.lang.String;@6df6e30f
+    capabilities = [Ljava.lang.String;@377419f8
+}
