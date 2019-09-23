5b8651d07de18f6df054edb1e0e7e4af6cea4d97
api <api@frontend-1>
2019-09-22 23:08:21 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000007_1569218897890/config_TestOrg-evenAnotherOrg-rjp3rv74xrn3xu43.tf

--- config_TestOrg-evenAnotherOrg-rjp3rv74xrn3xu43.tf
+++ config_TestOrg-evenAnotherOrg-rjp3rv74xrn3xu43.tf
@@ -8,19 +8,19 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = "Admin"
      lastName = "User"
      isActive = "true"
-     roleIds = [Ljava.lang.String;@2b7bfe6a
+     roleIds = [Ljava.lang.String;@14e8a82d
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = "Administrator"
     description = ""
     filterPredicate = "*"
-    users = [Ljava.lang.String;@10618bcf
-    capabilities = [Ljava.lang.String;@6076d968
+    users = [Ljava.lang.String;@73fdd409
+    capabilities = [Ljava.lang.String;@615d4163
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = "Analyst"
     description = ""
     filterPredicate = ""
-    users = [Ljava.lang.String;@3af168e0
-    capabilities = [Ljava.lang.String;@4617ea06
+    users = [Ljava.lang.String;@6e58ac7d
+    capabilities = [Ljava.lang.String;@5839af1b
 }
