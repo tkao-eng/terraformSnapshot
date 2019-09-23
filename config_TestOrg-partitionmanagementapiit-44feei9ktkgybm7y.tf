3427330aeccb4083cdf965fd54ecfc84637c519e
api <api@frontend-1>
2019-09-22 23:01:51 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000011_1569218508589/config_TestOrg-partitionmanagementapiit-44feei9ktkgybm7y.tf

--- config_TestOrg-partitionmanagementapiit-44feei9ktkgybm7y.tf
+++ config_TestOrg-partitionmanagementapiit-44feei9ktkgybm7y.tf
@@ -8,25 +8,25 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = "Admin"
      lastName = "User"
      isActive = "true"
-     roleIds = [Ljava.lang.String;@4d4beacf
+     roleIds = [Ljava.lang.String;@70286d0d
 }
 resource "sumologic_user" "terraformtestuserUser" {
     firstName = "test"
      lastName = "user"
      isActive = "true"
-     roleIds = [Ljava.lang.String;@6d76898b
+     roleIds = [Ljava.lang.String;@25203157
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = "Administrator"
     description = ""
     filterPredicate = "*"
-    users = [Ljava.lang.String;@1e6f4c40
-    capabilities = [Ljava.lang.String;@72ef80ed
+    users = [Ljava.lang.String;@2f579302
+    capabilities = [Ljava.lang.String;@7a41d86f
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = "Analyst"
     description = ""
     filterPredicate = ""
-    users = [Ljava.lang.String;@12c5135a
-    capabilities = [Ljava.lang.String;@589208f2
+    users = [Ljava.lang.String;@1c244bee
+    capabilities = [Ljava.lang.String;@3d6cc2ee
 }
