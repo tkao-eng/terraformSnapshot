2fc5b0cfed29dafd25c9619090cb9bb275520bc1
api <api@frontend-1>
2019-09-22 18:57:26 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000009_1569203843747/config.tf

--- config.tf
+++ config.tf
@@ -8,25 +8,19 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = Admin
      lastName = User
      isActive = true
-     roleIds = [000000000000000F]
-}
-resource "sumologic_user" "terraformtestuserUser" {
-    firstName = test
-     lastName = user
-     isActive = true
-     roleIds = [0000000000000010]
+     roleIds = [0000000000000011]
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = Administrator
     description = 
     filterPredicate = *
-    users = [00000000000001D8]
+    users = [00000000000001DA]
     capabilities = [dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding]
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = Analyst
     description = 
     filterPredicate = 
-    users = [00000000000001D9]
+    users = []
     capabilities = [viewCollectors]
 }
