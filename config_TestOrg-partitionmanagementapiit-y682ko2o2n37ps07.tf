418b7da525cec3d2eb4a9f5089a12e3c201bd8b8
api <api@frontend-1>
2019-09-22 23:39:54 -0700

    Updating Terraform configuration./tmp/terraform_snapshot_0000000000000008_1569220790869/config_TestOrg-partitionmanagementapiit-y682ko2o2n37ps07.tf

--- config_TestOrg-partitionmanagementapiit-y682ko2o2n37ps07.tf
+++ config_TestOrg-partitionmanagementapiit-y682ko2o2n37ps07.tf
@@ -8,25 +8,25 @@ resource "sumologic_user" "terraformAdminUserUser" {
     firstName = "Admin"
      lastName = "User"
      isActive = "true"
-     roleIds = 000000000000000F
+     roleIds = [000000000000000F]
 }
 resource "sumologic_user" "terraformtestuserUser" {
     firstName = "test"
      lastName = "user"
      isActive = "true"
-     roleIds = 0000000000000010
+     roleIds = [0000000000000010]
 }
 resource "sumologic_role" "terraformAdministratorRole" {
     name = "Administrator"
     description = ""
     filterPredicate = "*"
-    users = 00000000000001D8
-    capabilities = dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding
+    users = [00000000000001D8]
+    capabilities = [dataVolumeIndex, manageAuditDataFeed, manageAccessKeys, auditEventIndex, manageConnections, metricsExtraction, manageIndexes, shareDashboardWhitelist, manageFieldExtractionRules, searchAuditIndex, ipWhitelisting, manageMonitors, manageUsersAndRoles, metricsTransformation, manageBudgets, manageSaml, manageContent, manageSupportAccountAccess, shareDashboardOutsideOrg, managePasswordPolicy, shareDashboardWorld, manageCollectors, viewCollectors, manageDataVolumeFeed, manageS3DataForwarding]
 }
 resource "sumologic_role" "terraformAnalystRole" {
     name = "Analyst"
     description = ""
     filterPredicate = ""
-    users = 00000000000001D9
-    capabilities = viewCollectors
+    users = [00000000000001D9]
+    capabilities = [viewCollectors]
 }
